8c32bb74569d57f7fc08ba0b56591846e07c468d
Yajayra <yajayra.arauco@gmail.com>
2018-10-31 02:51:42 -0500

    gracias mi amor por tener paciencia al enseñarme bucles

---
+++ 30.10.2018.py
@@ -0,0 +1,21 @@
+edad = 0 
+
+while edad < 20:
+	#Edad = 20 
+	#print (edad)  --> 20
+	#print (type (edad))  --> int
+	#print (str(edad))  --> 20
+	#print (type (str(edad)))  --> str
+
+	#ejecuta esta línea
+	print (edad)
+	#print ("Tienes " + str (edad))
+	#print ("2Tienes", edad)
+	#Ejecuta esta lìnea - recordar que debe estar identado
+	edad = edad + 1
+#edad = str (edad)
+
+#print ("Hola" + "Mundo" + "1")
+#print ("Hola Mundo 1")
+#print ("Hola" + "Mundo " + str (1) )
+#print ("Hola" + "Mundo " + 1)
