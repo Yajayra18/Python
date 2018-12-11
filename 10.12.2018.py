fc88b43c121cb9f9afecdb58785c6d7f0345ca34
Yajayra <yajayra.arauco@gmail.com>
2018-12-11 00:06:25 -0500

    Aún quiero entender más de las clases y objetos

---
+++ 10.12.2018.py
@@ -0,0 +1,14 @@
+class Perro:
+	def __init__(self):
+		print ("soy una mascota")
+
+	def ladra(self, Ladra):
+		print ("Ladra")
+
+Firulais = Perro()
+Teodoro = Perro()
+
+Firulais.ladra("Wuauf")
+
+Teodoro.ladra("Wuauf Wuauf")
+	
