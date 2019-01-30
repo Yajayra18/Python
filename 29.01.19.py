19c3d3eef4533ae2db62c7c1e813217d8324ec49
Yajayra <yajayra.arauco@gmail.com>
2019-01-29 23:25:15 -0500

    tengo muchas dudas existenciales

---
+++ 29.01.19.py
@@ -0,0 +1,19 @@
+
+print ("Holi")
+
+#tuplas
+""" almacena datos que no pueden ser modificados una vez 
+creados se caracteríza por usar parentesis"""
+
+mi_tupla = ("te quiero", 7, 0.2, "dos mil dieciocho")
+print (mi_tupla)
+
+#se acceder a los datos mediante su índice
+print (mi_tupla[1:4])
+print (mi_tupla[2:])
+print (mi_tupla [:2])
+
+#dudas existenciales.
+""" si dice que se accede mediante su indice pareciese que [1:4] 
+según lo anterior 1 es indice y 4 es elemento y al mencionarlo 
+puede que se considere incluyendo eso.
