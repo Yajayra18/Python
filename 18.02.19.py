4110878ecbf4914a2a5925d53f60a7719068a7de
Yajayra <yajayra.arauco@gmail.com>
2019-02-18 23:21:04 -0500

    Repasando y avanzando

---
+++ 18.02.19.py
@@ -0,0 +1,32 @@
+#Repasando
+"""para Tuplas,se utiliza ...> Parentesis( )
+Para listas se utiliza -----> Corchetes [ ]
+Para diccionarios se utiliza ----> Llaves { }
+Notita, la tupla es perenne, los indices de la lista puede cambiar
+y al diccionario se le puede asignar manuelmente el ìndice
+"""
+tupla = ("Esto", 4, 7.2, "Estoy muy agradecida de que compartamos nuestras vidas")
+print (tupla [1:3])
+#Recordar que al imprimir la tupla es de la siguiente forma
+# [Inidice inicial: indice final -1]
+lista = ["cat", "Chat", 5, 7.2]
+print (lista [2:])
+diccionario = {'Pomme': 'Apple', 'Bonjour': "Buenos días", 'mon amor':'Joseph'}
+print (diccionario ['Bonjour'])
+print (diccionario ['mon amor'])
+
+#estructuras de control de flujo
+"""esta permite agrupar instrucciones de manera controlada.
+existes 2 estructuras de control
+- E de C. Condicionales
+- E de C. Iterativas"""
+
+#IDENTACIÓN
+#Es la sangria,  está conformado por 4 espacios en blanco.
+
+print ('identacion')
+# ENCODING
+"""Importante no omitirlas
+esta es es uan directva que se pone al inicio del archivo python 
+con el fin de indicar al sistema la odificacion utilizada en el archivo.
+....continuará1"""
