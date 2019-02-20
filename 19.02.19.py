3cb365eb86b308db695939c0a56532bca26b5f13
Yajayra <yajayra.arauco@gmail.com>
2019-02-19 22:05:48 -0500

    me gustó estudiar al aire libre

---
+++ 19.02.19.py
@@ -0,0 +1,87 @@
+"""Hola amor, este lugar es increible! 
+hoy estoy cerca a tu facultad... en paz
+"""
+#ENCODING
+print ("El Ñandu en un ñaño")
+# se supone que no deberìa de imprimir y para 
+# que imprima se tiene que poner lo siguiente
+# -*- coding: utf-8 -*-
+
+#ASIGNACIÓN MÚLTIPLE
+"""en una sola instrucción se declaran 3 variables
+"""
+Gato, alpaca, Tucan = "Costa", "Sierra", "Selva"
+print (Gato)
+print (alpaca)
+print (Tucan)
+
+#Contenido de una tupla
+mi_tupla = ("meow", 2019)
+maullido, año = mi_tupla
+print  (maullido)
+print (año)
+
+#Contenido de lista
+mi_lista = ["Chosica", "Lima"]
+distrito, provincia = mi_lista
+print (distrito)
+print (provincia)
+
+#Practicando
+animales =  ["Yalú", "Teodoro", "Bebito", "michi"]
+negro, caramelo, blanco, plomo = animales
+print (negro)
+
+
+"""ESTRUCTURA DE CONTROL DE FLUJO,
+esta nos permite saber si una 
+condicion cumple y así poner que acción va a ejecutar.
+
+"""
+#OPERADORES RELACIONALES, de comparación
+
+print (2 == 7)
+print ("empleado" != "obrero")
+print (24 < 25)
+print (26 > 26)
+print (24 <= 8)
+print (9 >= 12)
+
+#OPERADORES LÓGICOS
+"""EXITEN and(y), or (o)
+Notita importante: es exactamente igual a lógica
+por ejemplo: """
+
+b = (3 < 5) and (9 > 8)
+a = (5 == 3) and (8 > 9)
+
+print (b)
+
+print (a)
+
+c = (11 == 11) or (5 < 8)
+print (c)
+
+#Ejemplo
+#dinero invertido en compras
+g = 52
+if  g > 50:
+	print ("Rayos señorita no gaste más, que se quedará en banca rota")
+else:
+	print ("Estoy dentro del presupuesto")
+
+#Ejemplo 2
+#de es la hora
+d = 
+if d == 6:
+	print ("Ya es hora de levantarse linda")
+elif d == 7:
+	print ("hermosa el tiempo te quedará justooo")
+elif d == 8:
+	print ("ya valiste, perderás el trabajo de tus sueños por quedarte dormida T_T")
+else:
+	print ("Holi")
+
+
+
+
