a4ff7f1bfa9be538e17a107a68fb4f34fe2a0d06
Yajayra <yajayra.arauco@gmail.com>
2019-02-25 23:39:30 -0500

    Ejemplos de bucles, diferencia entre while y for, hechos por mi amor

---
+++ test.py
@@ -0,0 +1,19 @@
+#Sentencia while
+
+
+print("Ingresa un numero distinto de cero")
+print("e ingresa 1 para terminar el programa")
+i = int(input(":? "))
+suma = i
+while i != 1:
+    i = int(input(":? "))
+    suma += i
+print("la suma de todos los numeros ingresado es ",suma)
+
+# Cuidado con los bucles infinitos,
+# Verificar siempre que las sentencias
+# sean falsas en algun momento
+
+
+
+# Se
---
+++ test1.py
@@ -0,0 +1,10 @@
+#Sentencia for
+
+for x in "Yajayra":
+    print("%s Hola %s"%(x,x))
+
+
+# x = 1   ----- print("i%i"%x)  i1
+# x = 1.  ----- print("qwe %f"%x) qwe 1.0000
+# x = "1" ----- print("asd %s"%x)  asd 1
+# x = 1.  ----- print("hola %d"%x)  hola 1
