4920733295a4594c48d9633ae57a46ee87234293
Yajayra <yajayra.arauco@gmail.com>
2019-02-27 01:37:21 -0500

    inicio

---
+++ 26.02.19.py
@@ -0,0 +1,9 @@
+print ("MAYORES QUE EL ANTERIOR")
+a = int(input ("¿Cuántos valores va a introducir?"))
+if a >= 1:
+    b = int(input(("Escriba un número:"))
+    if b >= 1
+    c = int(input("Escriba un número más grande que %b"))
+    d = int(input("Escriba un número más grande que %c"))
+    e = int (input("Escriba
+else print ("Imposible!")
