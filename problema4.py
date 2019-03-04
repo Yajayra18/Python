08d36310345489eb2d673c2facd04eb79a3d73ab
Yajayra <yajayra.arauco@gmail.com>
2019-03-04 00:22:28 -0500

    falta incluir el factorial

---
+++ problema4.py
@@ -0,0 +1,9 @@
+print ("FACTORIAL")
+a = int(input("Escriba un número entero mayor que cero:"))
+factorial = 0
+if a <= 0:
+    print ("¡Le he pedido un número entero mayor que cero!")
+else:
+    for i in range(a+1):
+        factorial + 1= factorial
+    print ("El factorial de %i es %i"%(a,factorial))
