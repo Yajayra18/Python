940fcb8b0889e1e58dbbd0899c48842b45b4fb5a
Yajayra <yajayra.arauco@gmail.com>
2019-04-15 23:23:19 -0500

    Observaciones

---
+++ 15.04.19.py
@@ -0,0 +1,22 @@
+#crea una funcion intermedio(a,b) que a partir
+#de dos numeros, devuelva su punto
+#intermedio entre -12 y 24
+
+def intermedio(a,b):
+    print (0.5 * (a+b))
+
+intermedio(-12, 24)
+
+#Realiza una funcion separar(lista) que tome
+#na lista de numeros enteros y devuelva
+#dos listas ordenadas. La primera con los numeros
+#pares y la segunda con los numeros impares.
+
+def separar(lista):
+    lista = (1,2,3,4,5,6,7,8,9,10)
+    print (range (lista))
+    
+separar(lista)
+
+
+
