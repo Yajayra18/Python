d2cc424248a5e209d5065d7bc2d27705058bc032
Yajayra <yajayra.arauco@gmail.com>
2019-09-16 22:25:12 -0500

    Cartita para mi potito

---
+++ 09-09-19.py
@@ -0,0 +1,9 @@
+J = open("Cartita para el Poto", "w")
+Y = ""
+Y +="Hola amor, te cuento que hoy me puse a reflexionar\n"
+Y += "y me percate que me siento muy enamorada de ti. \n" 
+Y += "Amo tu forma de ser, y sobre todo tu esencia\n"
+Y += "Feliz 19 meses y 9 dias, mi potitomi\n"
+Y += "eres sumamente importante para mi"
+J.write(Y)
+J.close()
