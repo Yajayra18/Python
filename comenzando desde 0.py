793247cd46377f16afee4e5f05587c5be97ca139
Yajayra <yajayra.arauco@gmail.com>
2019-09-23 22:02:28 -0500

    Variables

---
+++ comenzando desde 0.py
@@ -0,0 +1,42 @@
+#Variables
+#La variable es un objeto donde puedo guardar informacion
+#Esta informacion puede ser un numero real o entero, un texto un boleano.
+
+#Numero Entero - Es mas conocido como Int.
+
+g = 16
+print(g)
+
+#no olvides que g, ahora es una variable que le se guardo la informacion --16
+#Incluso puedo saber si es un Int, poniendo lo siguiente:
+
+type(g)
+
+
+#Numero Real - Es mas conocido como Float.
+
+f = 5.5
+#Es un float por que en la variable f se guardo la informacion considerando los decimales.
+
+#Texto - Es mas conocido como String, e incluso es de vital importancia poner en texto entre comillas sea simple o doble.
+t = "Hola mundo, espero te ayude muchoe ste archivo - comenzando desde 0 "
+
+#Boleano - La verdad no es su forma facil pero es mas conocido como boleano
+#Con las respuestas que de da el boleano sea Verdadero o Falso
+#Se pueden tomar decisiones. Como en la vida real
+
+2 == 10 
+#es Falso por que 2 no es igual a 10
+
+#pero si lo llevamos a la vida real
+#si hoy hace frio entonces yo decido ponerme una chompa y si no pues no 
+#En este caso el boleano me permite tomar una decision
+hoy = "frio" 
+if hoy is "frio":
+   print ("me pondre una chompa")
+   else:
+       print("No me pondre una chompa")
+
+
+
+
